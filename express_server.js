1f854e1bd78e8142fa6de4831310b0c6268e797c
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-28 14:16:23 -0400

    add

--- express_server.js
+++ express_server.js
@@ -11,9 +11,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
+  
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+    const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };
+    res.render("urls_show", templateVars);
+  });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -23,7 +34,11 @@ app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
   app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+    //res.send("<html><body>Hello <b>World</b></body></html>\n");
+    const templateVars = { greeting: 'Hello World!' };
+    res.render("hello_world", templateVars);
+
+
   });
 
   app.get("/set", (req, res) => {
